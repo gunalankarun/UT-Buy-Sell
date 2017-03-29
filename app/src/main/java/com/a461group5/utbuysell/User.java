b721db9b1c818283b33400134f32a601994e6e94
irfanhasan <ihasan03@gmail.com>
2017-03-29 17:01:50 -0500

    User class created

---
+++ app/src/main/java/com/a461group5/utbuysell/User.java
@@ -0,0 +1,31 @@
+package com.a461group5.utbuysell;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by irfanhasan on 3/29/17.
+ */
+
+public class User {
+    String email;
+    String first_name;
+    String last_name;
+    Map<String, Boolean>  seller_posts;
+    Map<String, Boolean> buyer_posts;
+    Map<String, Boolean> chats;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String email, String first_name, String last_name) {
+        this.email = email;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        seller_posts = new HashMap<String, Boolean>();
+        buyer_posts = new HashMap<String, Boolean>();
+        chats = new HashMap<String, Boolean>();
+    }
+    
+}
