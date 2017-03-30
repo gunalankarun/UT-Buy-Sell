d7d148827263f700b623de44a7943bc8f2ab373e
Gunalan Karun <gunalan.karun@gmail.com>
2017-03-29 21:07:44 -0500

    Added Category Class

---
+++ app/src/main/java/com/a461group5/utbuysell/Category.java
@@ -0,0 +1,41 @@
+package com.a461group5.utbuysell;
+
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// [START post_class]
+@IgnoreExtraProperties
+public class Category {
+
+    public String name;
+    public String description;
+    public Map<String, Boolean> posts;
+
+    public Category() {
+        // Default constructor required for calls to DataSnapshot.getValue(Post.class)
+    }
+
+    public Category(String name, String description, String postid) {
+        this.name = name;
+        this.description = description;
+        this.posts = new HashMap<>();
+        this.posts.put(postid, true);
+    }
+
+    // [START post_to_map]
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("name", name);
+        result.put("description", description);
+        result.put("posts", posts);
+
+        return result;
+    }
+    // [END post_to_map]
+
+}
+// [END post_class]
