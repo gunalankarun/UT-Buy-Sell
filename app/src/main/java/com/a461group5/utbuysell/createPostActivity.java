ae56cf4a58014b18d38431ce7c21109974af230a
Gunalan Karun <gunalan.karun@gmail.com>
2017-03-29 22:30:16 -0500

    Implemented Creation of post and storing model in firebase

--- app/src/main/java/com/a461group5/utbuysell/createPostActivity.java
+++ app/src/main/java/com/a461group5/utbuysell/createPostActivity.java
@@ -3,32 +3,99 @@ package com.a461group5.utbuysell;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class createPostActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    private EditText mtitleField;
+    private EditText mdescriptionField;
+    private EditText mpriceField;
+    private EditText mcategoriesField;
+    private Button mSubmitButton;
+    private Button mCancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         initUI();
     }
 
     private void initUI() {
 
-        Button logOutButton = (Button) findViewById(R.id.create_post_cancel);
-        TextView ProfileHeader = (TextView) findViewById(R.id.create_post_header);
-        ProfileHeader.setText("Title of Item");
+        mtitleField = (EditText) findViewById(R.id.create_post_title);
+        mdescriptionField = (EditText) findViewById(R.id.create_post_description);
+        mpriceField = (EditText) findViewById(R.id.create_post_price);
+        mcategoriesField = (EditText) findViewById(R.id.create_post_categories);
 
-        logOutButton.setOnClickListener(new View.OnClickListener() {
+        mSubmitButton = (Button) findViewById(R.id.create_post_submit);
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitPost();
+
+            }
+        });
+
+        mCancelButton = (Button) findViewById(R.id.create_post_cancel);
+        mCancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent myIntent = new Intent(createPostActivity.this, MainActivity.class);
                 createPostActivity.this.startActivity(myIntent);
             }
         });
+    }
+
+    private void submitPost() {
+        // Disable submit button to avoid double submits
+        mSubmitButton.setEnabled(false);
+
+        String title = mtitleField.getText().toString().trim();
+        String description = mdescriptionField.getText().toString().trim();
+        String priceText = mpriceField.getText().toString().trim();
+        String categoriesText = mcategoriesField.getText().toString().trim();
+
+        // All are required
+        if (TextUtils.isEmpty(title) || TextUtils.isEmpty(description) || TextUtils.isEmpty(priceText) || TextUtils.isEmpty(categoriesText)) {
+            Toast.makeText(createPostActivity.this, "All fields are required",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        int price = Integer.parseInt(priceText);
+        String[] categories = categoriesText.split(",");
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String key = mDatabase.child("posts").push().getKey();
+        Post post = new Post(title, description, user.getUid(), price, categories);
+        mDatabase.child("posts").child(key).setValue(post);
+
+        // TODO: Update user array with post information
+
+        // Reenable Button
+        mSubmitButton.setEnabled(true);
+
+        Toast.makeText(createPostActivity.this, "Created Post!",
+                Toast.LENGTH_SHORT).show();
+        Intent myIntent = new Intent(createPostActivity.this, MainActivity.class);
+        createPostActivity.this.startActivity(myIntent);
+
+
+
     }
 }
