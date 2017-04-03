d945eb99619a4a1e24b2cb064efab421cf804be5
Gunalan Karun <gunalan.karun@gmail.com>
2017-04-02 19:14:39 -0500

    Implemented Initial Template for View Post

---
+++ app/src/main/java/com/a461group5/utbuysell/ViewPostActivity.java
@@ -0,0 +1,41 @@
+package com.a461group5.utbuysell;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class ViewPostActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    private StorageReference mStorageRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_post);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        Bundle bundle = getIntent().getExtras();
+        String messageId = bundle.getString("messageId");
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        TextView title = (TextView) findViewById(R.id.view_post_title);
+        title.setText("Title");
+
+    }
+}
