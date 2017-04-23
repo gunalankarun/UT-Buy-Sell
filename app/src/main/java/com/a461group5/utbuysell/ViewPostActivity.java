3aa06e8f1c149bece84a87266932587158fd30a4
Kevin Liang <kevin.liang@utexas.edu>
2017-04-23 17:38:00 -0500

    updated viewpostpage to include meeting location/time

--- app/src/main/java/com/a461group5/utbuysell/ViewPostActivity.java
+++ app/src/main/java/com/a461group5/utbuysell/ViewPostActivity.java
@@ -15,6 +15,8 @@ public class ViewPostActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
     private StorageReference mStorageRef;
+    private EditText meeting_location;
+    private EditText meeting_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +34,29 @@ public class ViewPostActivity extends AppCompatActivity {
 
     private void initUI() {
 
+
+        // all set text or set image will be set based on data from firebase
         TextView item_name = (TextView) findViewById(R.id.view_post_name);
         item_name.setText("Item Name");
-        TextView description = (TextView) findViewById(R.id.view_post_description);
-        description.setText("Description of Item or Items being sold.");
         TextView seller_name = (TextView) findViewById(R.id.view_post_seller);
         seller_name.setText("Seller Name");
+        TextView item_price = (TextView) findViewById(R.id.view_post_price);
+        item_price.setText("Item Price");
+        TextView description = (TextView) findViewById(R.id.view_post_description);
+        description.setText("Description of Item or Items being sold.");
+
+        //  image related
+        // still not sure how to add in images or how to check if the xml image code is correct
         ImageView image = (ImageView) findViewById(R.id.view_post_picture1);
+        //image.setImageURI();
+
+        TextView item_tags = (TextView) findViewById(R.id.view_post_tags);
+        item_tags.setText("Comma Seperated Item Tags");
+
+        meeting_location = (EditText) findViewById(R.id.view_post_meeting_location);
+
+        meeting_time = (EditText) findViewById(R.id.view_post_meeting_time);
+
 
 
     }
