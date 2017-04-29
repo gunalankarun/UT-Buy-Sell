9866c7432520ea7c9e72dc9cd6df8333534805ef
Shamma Kabir <shammakabir@utexas.edu>
2017-04-29 17:55:49 -0500

    posts

--- app/src/main/java/com/a461group5/utbuysell/fragments/PageFragment.java
+++ app/src/main/java/com/a461group5/utbuysell/fragments/PageFragment.java
@@ -29,6 +29,7 @@ import com.a461group5.utbuysell.R;
 import com.a461group5.utbuysell.ViewPostActivity;
 import com.a461group5.utbuysell.adapters.ListingsAdapter;
 import com.a461group5.utbuysell.models.InboxEntry;
+import com.a461group5.utbuysell.models.Post;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -36,7 +37,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ChildEventListener;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -59,6 +61,7 @@ public class PageFragment extends Fragment {
     private ArrayList<InboxEntry> inboxEntries;
     private ListView inboxListView;
     private Context inboxContext;
+    ArrayList<Post> itemsData = new ArrayList<Post>();
     //////////////////////////////////////////////////
 
     private enum Type {
@@ -197,10 +200,36 @@ public class PageFragment extends Fragment {
             ListingsHeader.setText("Transactions");
         }
 
-        ArrayList<String> itemsData = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
-            itemsData.add("Fragment " + getArguments().getInt("index", -1) + " / Item " + i);
-        }
+
+        Query postsQuery = mDatabase.child("posts").orderByKey();
+
+
+        postsQuery.limitToFirst(50).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (Post snapshot : dataSnapshot.getValue(Post.class)) {
+                    Post post = snapshot;
+                    itemsData.add(post);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+        postsQuery.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot) {
+                for (Post snapshot : dataSnapshot.getValue(Post.class)) {
+                    Post post = snapshot;
+                    itemsData.add(post);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+
 
         ListingsAdapter adapter = new ListingsAdapter(itemsData);
         recyclerView.setAdapter(adapter);
